d47e68552fe5692411d24e59ac81347cbb031916
NoHaWeb <onlinenorbert@gmail.com>
2024-04-17 12:03:29 +0200

    Style changes -> Welcome site
    Assign main header from H2 to H1 and align to center

--- Files/Agenor1/resources/views/welcome.blade.php
+++ Files/Agenor1/resources/views/welcome.blade.php
@@ -21,9 +21,9 @@
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                    <h2 class="text-xl font-semibold text-black dark:text-white"> Agenor</h2>
+                    
                         <div class="flex lg:justify-center lg:col-start-2">
-                        
+                            <h1 class="text-center font-semibold text-black dark:text-white" style="font-size: 3rem">Agenor</h1>    
                         </div>
                         @if (Route::has('login'))
                             <nav class="-mx-3 flex flex-1 justify-end">
