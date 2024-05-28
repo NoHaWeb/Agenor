bb506342fd55234351f6fc1599cdc5459dd9996f
NoHaWeb <onlinenorbert@gmail.com>
2024-05-28 16:57:33 +0200

    file-ok eredeti nevének megőrzése

--- Files/Agenor1/app/Http/Controllers/UploadController.php
+++ Files/Agenor1/app/Http/Controllers/UploadController.php
@@ -6,23 +6,23 @@ use Illuminate\Http\Request;
 
 class UploadController extends Controller
 {
-     public function index()
+    public function index()
     {
+
         return view('file-upload');
     }
 
     public function store(Request $request)
     {
-        
         $validatedData = $request->validate([
-         'file' => 'required|mimes:doc,docx,pdf,txt,json,xlsx,xls|max:2048',
+            'file' => 'required|mimes:doc,docx,pdf,txt,json,xlsx,xls|max:2048',
         ]);
 
-        $path = $request->file('file')->store('public/files');
+        $file = $request->file('file');
+        $originalName = $file->getClientOriginalName();
 
-        //write the logic here to store file in database
+        $path = $file->storeAs('public/files', $originalName);
 
-        return redirect('dashboard')->with('status', 'File Has been uploaded successfully in Agenor!');
-
+        return redirect('dashboard')->with('status', 'File has been uploaded successfully in Agenor!');
     }
 }
