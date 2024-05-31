52f6517485063f76b70a63bdc9fa35dd8b782acd
NoHaWeb <111111079+NoHaWeb@users.noreply.github.com>
2024-05-31 20:27:46 +0100

    regisztráció érvényes adatokkal

---
+++ Files/Agenor1/cypress/e2e/register_noha_online.cy.js
@@ -0,0 +1,21 @@
+describe('Regisztrációs teszt', () => {
+    it('Sikeres regisztráció', () => {
+        
+      cy.visit('https://nohaweb.com/Agenor/')
+      
+      cy.get('a[href="https://nohaweb.com/Agenor/public/register"]').click()
+  
+      cy.get('input[name="name"]').type('Uj User')
+      
+      cy.get('input[name="email"]').type('uj@user.com')
+  
+      cy.get('input[name="password"]').type('ujjuser123')
+
+      cy.get('input[name="password_confirmation"]').type('ujjuser123')
+  
+      cy.get('button[type="submit"]').click()
+  
+      cy.url().should('include', '/dashboard')
+      cy.contains('logged in')
+    })
+  })
